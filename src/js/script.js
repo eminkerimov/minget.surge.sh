c9d4bde1a48d34e243356de7ec5f061ea4a33780
emin.kerimov <kerimov_emin1@mail.ru>
2020-11-07 21:19:40 +0400

    11.07.2020

--- src/js/script.js
+++ src/js/script.js
@@ -9,3 +9,53 @@ function myFunction() {
 }
 
 
+$(document).ready(function(){
+    $('.slick').slick({
+        arrows:false,
+        dots:true,
+        slidesToShow:2,
+        slidesToScroll:2,
+        speed:2000,
+        infinite:true,
+        autoplay:true,
+        autoplaySpeed:2000,
+        pauseOnHover:true,
+        pauseOnDotsHover:true,
+        responsive:[
+            {
+                breakpoint:740,
+                settings:{
+                    slidesToShow:1,
+                    slidesToScroll:1
+                }
+            }
+        ]
+    });
+    $('.slider').slick({
+        infinite:true,
+        autoplay:true,
+        autoplaySpeed:2000,
+        pauseOnHover:false,
+        pauseOnDotsHover:false,
+        arrows:false,
+        fade:true,
+    });
+    $('.slideshow').slick({
+        arrows:true,
+        dots:true,
+        slidesToShow:2,
+        slidesToScroll:2,
+        speed:1500,
+        infinite:true,
+        appendArrows:$('.arrow'),
+        responsive:[
+            {
+                breakpoint:740,
+                settings:{
+                    slidesToShow:1,
+                    slidesToScroll:1
+                }
+            }
+        ]
+    });
+});
